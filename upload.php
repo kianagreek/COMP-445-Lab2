4a63766fcab002ac91de48cce5b383358d0df730
RyanHaniff <haniffryan.hr@gmail.com>
2023-04-09 20:35:22 -0400

    fixed the issues

--- upload.php
+++ upload.php
@@ -14,22 +14,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_FILES["video"])) {
   }
 }
 ?>
-
-
-/*  <?php
-// $servername = 'ruc353.encs.concordia.ca:3306';
-// $database = 'ruc353_1';
-// $username = 'ruc353_1';
-// $password = 'DROP_TAB';
-
-// $servername = 'localhost:3306';
-// $username = 'root';
-// $password = '';
-// $database = 'mysql';
-
-// try {
-    // $conn = new PDO("mysql:host=$servername;dbname=$database;", $username, $password);
-// } catch (PDOException $e) {
-    // die('Connection Failed: ' . $e->getMessage());
-// }
-// ?> */
